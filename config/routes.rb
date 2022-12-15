5587c16608585ca99ba849f58de647d322e6c1d3
hapihapi777 <rock666gemini69@gmail.com>
2022-12-15 20:06:54 +0000

    test Hello, World

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "application#goodbye"
+  root "application#hello"
+  # root "application#goodbye"
+  
 end
