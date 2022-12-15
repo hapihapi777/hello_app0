73fdf29dad9b7ee9df0525463d6628589e976077
hapihapi777 <rock666gemini69@gmail.com>
2022-12-15 19:47:51 +0000

    test Hello, World

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
     render html: "¡hola, mundo!"
   end
   
-  def goodbye
-    render html: "goodbye, world!"
-  end
+  # def goodbye
+  #   render html: "goodbye, world!"
+  # end
 end
